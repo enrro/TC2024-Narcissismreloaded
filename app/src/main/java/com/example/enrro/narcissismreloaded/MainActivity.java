c74f0696575b519b31e0df1af989e47f00c2bc52
Enrro <edgarbg585@gmail.com>
2017-02-17 00:14:17 -0600

    Main activity now saves to the xml file the properties, and load them at the moment the activity starts. It was revealing the fact that you must set the activity first before saving it. IKR

--- app/src/main/java/com/example/enrro/narcissismreloaded/MainActivity.java
+++ app/src/main/java/com/example/enrro/narcissismreloaded/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.enrro.narcissismreloaded;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private static final String PROPERTIES_FILE = "properties.xml";
+    Properties properties;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        File file = new File(getFilesDir(), PROPERTIES_FILE);
+        properties = new Properties();
+
+        try {
+            if (file.exists()){
+                FileInputStream fis = openFileInput(PROPERTIES_FILE);
+                properties.loadFromXML(fis);
+                editText.setText(properties.getProperty("demo"));
+                fis.close();
+            }else{
+
+            }
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+
+
     }
+
+    private void saveProperties() throws IOException{
+        properties.setProperty("demo", editText.getText().toString());
+        FileOutputStream fos = openFileOutput(PROPERTIES_FILE, Context.MODE_PRIVATE);
+        properties.storeToXML(fos, null);
+        fos.close();
+    }
+
+    public void saveToStorage(View v){
+        try{
+            saveProperties();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+        Toast.makeText(this, "SAVED TO STORAGE", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
